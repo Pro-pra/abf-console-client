3f606198df9092377acfe2550d54ee370110a900
Pro-pra <pro2_2000@mail.ru>
2025-10-24 15:26:02 +0900

    change order build arch for Rosa platform

--- abf.py
+++ abf.py
@@ -1412,20 +1412,23 @@ def build(return_ids=False):
                 exit(1)
             arches.append(a)
     else:
-        try_arches = ['i586','i686','x86_64']
+        try_arches = ['x86_64', 'aarch64']
 
         # rosa2014.1, rosa2016.1 etc. (Rosa Fresh), but not e.g. rosa-server75 (Rosa-RHEL)
         if str(build_repositories[0]).find("rosa2019.0") >= 0 or str(build_repositories[0]).find("rosa2016.1") >= 0:
-            try_arches = ['i586', 'x86_64']
+            try_arches = ['x86_64','i586']
 
         if str(build_repositories[0]).find("rosa2019.05") >= 0:
-            try_arches = ['i686', 'x86_64']
+            try_arches = ['x86_64', 'i686']
 
-        if str(build_repositories[0]).find("rosa2021.1") >= 0 or str(build_repositories[0]).find("rosa13") >= 0:
-            try_arches = ['i686', 'x86_64', 'aarch64', 'riscv64', 'loongarch64']
+        if str(build_repositories[0]).find("rosa2021.1"):
+            try_arches = ['x86_64', 'aarch64', 'i686']
+
+        if str(build_repositories[0]).find("rosa2023.1") >= 0 or str(build_repositories[0]).find("rosa13") >= 0:
+            try_arches = ['x86_64', 'aarch64', 'i686', 'riscv64', 'loongarch64']
 
         if str(build_repositories[0]).find("rosa2021.15") >= 0:
-            try_arches = ['i686', 'x86_64', 'aarch64']
+            try_arches = ['x86_64', 'aarch64', 'i686']
 
         if str(build_repositories[0]).find("openmandriva") >= 0 or str(build_repositories[0]).find("4.2") >= 0:
             try_arches = ['x86_64', 'znver1', 'aarch64']
